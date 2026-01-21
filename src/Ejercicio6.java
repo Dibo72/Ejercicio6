32e04600328b7c33f4d9f47706fc0fc89ce48ec4
Dibo72 <humanida133@gmail.com>
2026-01-21 12:37:03 +0100

    Ejecicio comentado

--- src/Ejercicio6.java
+++ src/Ejercicio6.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        //listas sin ordenar
+        List<String> nombres = new ArrayList<>();
+        nombres.add("Pedro");
+        nombres.add("Ana");
+        nombres.add("Luis");
+        nombres.add("Ana");
+        nombres.add("Zaira");
+        nombres.add("Pedro");
+
+        //la psamos a hashset para eliminar dubplicados
+        Set<String> setSinDuplicados = new HashSet<>(nombres);
+
+        //lo volovemos a pasar a lista para ordenarla
+        List<String> listaFinal = new ArrayList<>(setSinDuplicados);
+        Collections.sort(listaFinal);
+
+
+        System.out.println(listaFinal);
     }
 }
